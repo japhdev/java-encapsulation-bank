3343a725617b71b1326af406b907e93e73443b09
Alan <133844140+Alanph18@users.noreply.github.com>
2026-07-06 02:59:45 -0600

    feat: Agregar clase PruebaBanco con método main para pruebas de CuentaBancaria

---
+++ src/PruebaBanco.java
@@ -0,0 +1,15 @@
+import java.util.Currency;
+
+public class PruebaBanco {
+    public static void main(String[] args){
+
+        // 1. Crear una cuenta
+        CuentaBancaria cuenta = new CuentaBancaria("123-313", "Alan Hernandez", 1000.0);
+
+        // 2.Mostras Informacion de la cuenta
+        System.out.println("=====INFORMACION DE LA CUENTA=======");
+        System.out.println("Titular: " + cuenta.getTitular());
+        System.out.println("Numero de Cuenta: " + cuenta.getNumeroCuenta());
+        System.out.println("Saldo inicial: $: " + cuenta.consultarSaldo());
+    }
+}
