a7099de4d1be857057158e15443b758a2c01059d
Alan <133844140+Alanph18@users.noreply.github.com>
2026-07-06 17:16:08 -0600

    refactor: simplifica la lógica de tienesSaldoSuficiente.

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -1,12 +1,12 @@
 public class CuentaBancaria {
-    
+
     // Atributos PRIVATE
     private String numeroCuenta;
     private double saldo;
     private String titular;
 
     // Constructor
-    CuentaBancaria(String numeroCuenta, String titular, double saldoInicial){
+    CuentaBancaria(String numeroCuenta, String titular, double saldoInicial) {
         this.numeroCuenta = numeroCuenta;
         this.titular = titular;
         this.saldo = saldoInicial;
@@ -14,58 +14,45 @@ public class CuentaBancaria {
     }
 
     // METODO depositar
-    public void depositar(double cantidad){
-        if(cantidad > 0){
+    public void depositar(double cantidad) {
+        if (cantidad > 0) {
             saldo += cantidad;
             System.out.println("Deposito exitoso. Nuevo saldo: $" + saldo);
-        }else{
+        } else {
             System.out.println("Cantidad invalida");
         }
-        
 
     }
 
     // METODO retirar (void)
-    public void retirar(double cantidad){
+    public void retirar(double cantidad) {
 
-        if (tieneSaldoSuficiente(cantidad)){
+        if (tieneSaldoSuficiente(cantidad)) {
             saldo -= cantidad;
             System.out.println("Retiro exitoso. Nuevo saldo: $" + saldo);
-
-            
-        } else {System.out.println("Saldo insuficiente. Saldo actual: " + saldo);
-
+        } else {
+            System.out.println("Saldo insuficiente. Saldo actual: " + saldo);
         }
-        
-        
-        
     }
 
     // METODO consultarSaldo (double)
-    public double consultarSaldo(){
+    public double consultarSaldo() {
         return saldo;
     }
 
     // METODO getTitular (String)
-    public String getTitular(){
+    public String getTitular() {
         return titular;
     }
 
     // METODO getNumeroCuenta (String)
-    public String getNumeroCuenta(){
+    public String getNumeroCuenta() {
         return numeroCuenta;
     }
 
     // METODO tieneSaldoSuficiente (boolean)
-    public boolean tieneSaldoSuficiente(double cantidad){
-        if(saldo >= cantidad){
-            return true;
-        }
-        else{
-            return false;
-        }
-            
+    public boolean tieneSaldoSuficiente(double cantidad) {
+        return saldo >= cantidad;
     }
-
 
 }
