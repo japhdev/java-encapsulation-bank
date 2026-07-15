fae042570086e41bd542a8857eeba84d67732f4a
Alan <133844140+Alanph18@users.noreply.github.com>
2026-07-15 00:35:12 -0600

    feat: actualizar PruebaBanco para crear y mostrar información de dos cuentas

--- src/PruebaBanco.java
+++ src/PruebaBanco.java
@@ -4,29 +4,35 @@ import java.util.Currency;
 public class PruebaBanco {
     public static void main(String[] args){
 
-        // 1. Crear una cuenta
-        CuentaBancaria cuenta = new CuentaBancaria("Alan Pablo", 1200);
+        // 1. Creacion de 2 cuentas
+        CuentaBancaria cuenta1 = new CuentaBancaria("Alan Pablo", 1000);
+        CuentaBancaria cuenta2 = new CuentaBancaria("Josue Hernandez", 500);
 
-        // 2.Mostras Informacion de la cuenta
-        System.out.println("=====INFORMACION DE LA CUENTA=======");
-        System.out.println("Titular: " + cuenta.getTitular());
-        System.out.println("Numero de Cuenta: " + cuenta.getNumeroCuenta());
-        System.out.println("Saldo inicial: $: " + cuenta.consultarSaldo());
+
+        // 2.Mostras informacion inicial
+        cuenta1.mostrarInformacion();
+        cuenta2.mostrarInformacion();
 
         // 3.Deposito de dinero
         System.out.println("\n ===DEPOSITO===");
-        cuenta.depositar(500.0);
+        cuenta1.depositar(500.0);
 
         // 4. Retiro de dinero
         System.out.println("\n ===RETIRO===");
-        cuenta.retirar(400.0);
+        cuenta1.retirar(400.0);
 
         // 5. Intento de retiro mayor al saldo disponible 
         System.out.println("\n ===RETIRO EXCESIVO===");
-        cuenta.retirar(2000.0);
+        cuenta1.retirar(2000.0);
 
-        // 6. Mostrar saldo final
-        System.out.println("\n=== SALDO FINAL ===");
-        System.out.println("Saldo actual: $" + cuenta.consultarSaldo());
+        // 6. Transferir    
+        System.out.println("\n===TRANSFERENCIA===");
+        cuenta1.transferir(cuenta2, 300);
+
+
+        // 7. Mostrar estado final
+        System.out.println("\n=== ESTADO FINAL ===");
+        cuenta1.mostrarInformacion();
+        cuenta2.mostrarInformacion();
     }
 }
