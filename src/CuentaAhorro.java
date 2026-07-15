e99b63f1051d42fc0e3f78185cdcfee2472168c9
Alan <133844140+Alanph18@users.noreply.github.com>
2026-07-15 01:01:43 -0600

    feat: implementar clase CuentaAhorro con lógica de retiros maximo 3 intentos.

--- src/CuentaAhorro.java
+++ src/CuentaAhorro.java
@@ -1,4 +1,27 @@
 // Clase hija
-public class CuentaAhorro {
+public class CuentaAhorro extends CuentaBancaria {
+
+    private double tasaInteresAnual;
+    private int retirosPermitidos = 3;
+    private int retirosRealizados = 0;
+
+    CuentaAhorro(String titular, double saldoInicial, double tasaInteresAnual) {
+        super(titular, saldoInicial);
+    }
+
+    @Override
+    public boolean retirar(double cantidad){
+        if (retirosRealizados < retirosPermitidos) {
+            super.retirar(cantidad);
+            retirosRealizados += 1;
+            return true;
+        }else{ 
+            System.out.println("Excediste el numero de intentos permitido");
+            return false;
+        }
+    }
+
+
+
     
 }
