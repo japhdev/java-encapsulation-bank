8d666ec9546fbae23d9ff85adfbe56438b23e716
Alan <133844140+Alanph18@users.noreply.github.com>
2026-07-26 23:36:04 -0600

    feat: constructor en CuentaInversion

--- src/CuentaInversion.java
+++ src/CuentaInversion.java
@@ -12,6 +12,23 @@ public class CuentaInversion extends CuentaBancaria{
     private int diasTranscurridos = 0;
     private boolean activa = true;
 
+        /**
+     * Crea una nueva cuenta de inversion con un titular, un saldo inicial, tasa de interés anual y un plazo de días
+     * de inversion.
+     *
+     * @param titular           Nombre del titular de la cuenta.
+     * @param saldoInicial      Saldo con el que se crea la cuenta.
+     * @param tasaInteresAnual  Tasa de interés anual
+     * @param plazoDias         Plazo de días
+     * 
+     */
+    public CuentaInversion(String titular, double saldoInicial, double tasaInteresAnual, int plazoDias){
+
+        super(titular, saldoInicial);
+        this.tasaInteresAnual = tasaInteresAnual;
+        this.plazoDias = plazoDias;
+    }
+
 
 
 }
