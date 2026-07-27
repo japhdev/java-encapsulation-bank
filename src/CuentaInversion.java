ee25a721f0a5d433431cf8d5f13bf77d04e4307a
Alan <133844140+Alanph18@users.noreply.github.com>
2026-07-26 23:32:23 -0600

    refactor: implementar la clase CuentaInversion heredando de CuentaBancaria y definir atributos privados

--- src/CuentaInversion.java
+++ src/CuentaInversion.java
@@ -1,5 +1,17 @@
-// Clase hija
-public class CuentaInversion {
+/**
+ * Representa una cuenta de inversion que hereda de {@code CuentaBancaria}.
+ * 
+ * @author Josue Alan Pablo Hernandez
+ * @version 1.0
+ */
+public class CuentaInversion extends CuentaBancaria{
+
+    // atributos privados
+    private double tasaInteresAnual = 0;
+    private int plazoDias = 0;
+    private int diasTranscurridos = 0;
+    private boolean activa = true;
+
 
 
 }
